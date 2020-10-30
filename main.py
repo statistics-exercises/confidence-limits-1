2eeb99498a02153b295c19d5bdec570fe9233293
Gareth Tribello <gareth.tribello@gmail.com>
2020-10-30 14:24:49 +0000

    Create main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+def uniform_discrete(a,b) :
+  # Your code goes here
+  
+
+
+print( uniform_discrete(1,6), uniform_discrete(1,6), uniform_discrete(1,6), uniform_discrete(1,6) )
