ce3968fc06466655bde49daa471d9a499ace463c
Gareth Tribello <gareth.tribello@gmail.com>
2021-05-11 15:57:17 +0100

    Added answers

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import numpy as np
 
 def uniform_discrete(a,b) :
   # Your code goes here
-  
+  return np.floor( (b+1-a)*np.random.uniform(0.1) ) + a 
 
 
 print( uniform_discrete(1,6), uniform_discrete(1,6), uniform_discrete(1,6), uniform_discrete(1,6) )
